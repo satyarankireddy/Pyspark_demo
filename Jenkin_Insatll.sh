db9b6be5c788a58854a4c234d8a16374d090ca79
satyarankireddy <43640135+satyarankireddy@users.noreply.github.com>
2020-07-08 17:28:58 +0530

    Create Jenkin_Insatll.sh

---
+++ Jenkin_Insatll.sh
@@ -0,0 +1,8 @@
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum -y install jenkins
+
+sudo service jenkins start
+
+sudo tail -f -n 100 /var/log/jenkins/jenkins.log
+
