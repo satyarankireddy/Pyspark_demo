4cc6aa0fd292326ebddd3a1bf22bed3464b32062
Ubuntu <ubuntu@ip-172-31-20-103.ap-south-1.compute.internal>
2020-07-16 14:08:03 +0000

    dpen

--- pyspark_dependency.sh
+++ pyspark_dependency.sh
@@ -1,14 +1,14 @@
-cd /app/PySpark-Boilerplate/
+#!/bin/bash
+  cd /app/PySpark-Boilerplate/
 mkdir ./dist
  cp ./src/main.py ./dist
  cd ./src && zip -x main.py -r ../dist/jobs.zip .
  cd /app/PySpark-Boilerplate/
-  pip3 install -r requirements.txt -t ./src
+  pip2 install -r requirements.txt -t ./src
 rm -rf dist
 mkdir ./dist
  cp ./src/main.py ./dist
  cd ./src && zip -x main.py -x \*libs\* -r ../dist/jobs.zip .
 cd /app/PySpark-Boilerplate/ 
-cd dist && spark-submit --py-files jobs.zip main.py --job wordcount
-  
+cd dist && spark-submit --py-files jobs.zip main.py --job wordcount > count.txt
 
